eaa755b13a87e095db824d8591db8a21c8674688
Matt <97047080+mattxmade@users.noreply.github.com>
2023-06-27 17:50:16 +0100

    Create search function; return results based on search params

---
+++ src/utils/faultSearch.ts
@@ -0,0 +1,30 @@
+import brands from "../../data/brands";
+
+const faultSearch = (string: string) => {
+  const matches: FaultItem[] = [];
+  const exactMatch: FaultItem[] = [];
+  const brandMatches: FaultItem[] = [];
+
+  const searchQuery = string.toLowerCase();
+  const queryList = searchQuery.trim().split(/\s+/);
+
+  brands.map((brand: BrandItem) => {
+    const brandName = brand.name.toLowerCase();
+
+    brand.faultCodes.map((item) => {
+      queryList.includes(brandName)
+        ? queryList.some((query) => query === item.faultCode.toLowerCase())
+          ? exactMatch.push(item)
+          : brandMatches.push(item)
+        : queryList.some((query) => query === item.faultCode.toLowerCase()) &&
+          matches.push(item);
+    });
+  });
+
+  if (exactMatch.length) return exactMatch;
+  if (brandMatches.length) return brandMatches;
+
+  return matches.length ? matches : null;
+};
+
+export default faultSearch;
