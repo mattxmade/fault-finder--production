7971d2c52387c6cfc5923e3be43e015a533eb5c3
Matt <97047080+mattxmade@users.noreply.github.com>
2023-06-28 19:05:50 +0100

    Create Component; add elements + inline styling; create toggle logic + callback

---
+++ src/components/ToggleSwitch.tsx
@@ -0,0 +1,44 @@
+import React, { DetailedHTMLProps, useCallback, useState } from "react";
+
+type Props = {
+  callback: () => void;
+} & JSX.IntrinsicElements["button"];
+
+const ToggleSwitch = ({ callback, ...props }: Props) => {
+  const [toggle, setToggle] = useState(false);
+
+  const onClick = useCallback(() => {
+    setToggle(!toggle);
+    callback();
+  }, [toggle]);
+
+  return (
+    <button
+      {...props}
+      style={{
+        width: "5rem",
+        height: "2.6rem",
+        position: "relative",
+        borderRadius: "2rem",
+        padding: "0.1rem",
+        border: "0.2rem solid lightgrey",
+      }}
+      onClick={onClick}
+    >
+      <div
+        style={{
+          width: "50%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          borderRadius: "100%",
+          backgroundColor: "lightblue",
+          transition: "0.3s",
+          transform: toggle ? "translate(2.2rem, 0rem)" : "translate(0, 0rem)",
+        }}
+      />
+    </button>
+  );
+};
+
+export default ToggleSwitch;
