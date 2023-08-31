245d36528ca02c438d072047c04583a3c890ddbc
Matt <97047080+mattxmade@users.noreply.github.com>
2023-08-31 17:21:16 +0100

    Add vite config; configure output

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  // ...
+  build: {
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
