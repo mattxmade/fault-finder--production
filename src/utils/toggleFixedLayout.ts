c8e61780dc047eb9b673354b8c33588cc37a985f
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-19 23:22:21 +0100

    Add fixed layout CSS util func

---
+++ src/utils/toggleFixedLayout.ts
@@ -0,0 +1,41 @@
+const toggleFixedLayout = (state: boolean, scrollYPosition: number) => {
+  const html = document.querySelector("html");
+  const body = document.querySelector("body");
+
+  if (!html || !body) return;
+
+  if (state) {
+    // html
+    [
+      { property: "scroll-behavior", value: "auto" },
+      //@ts-ignore
+    ].forEach((style) => (html.style[style.property] = style.value));
+
+    // body
+    [
+      { property: "position", value: "fixed" },
+      { property: "padding-right", value: "17px" },
+      { property: "top", value: -scrollYPosition + "px" },
+      { property: "left", value: "0px" },
+      { property: "right", value: "0px" },
+      { property: "height", value: "auto" },
+      //@ts-ignore
+    ].forEach((style) => (body.style[style.property] = style.value));
+  }
+
+  if (!state) {
+    // html
+    ["padding-right", "scroll-behavior"].forEach((property) =>
+      html.style.removeProperty(property)
+    );
+
+    // body
+    ["position", "padding-right", "top", "left", "right", "height"].forEach(
+      (property) => body.style.removeProperty(property)
+    );
+
+    window.scroll(0, scrollYPosition);
+  }
+};
+
+export default toggleFixedLayout;
