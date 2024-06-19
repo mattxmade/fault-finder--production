537e0f877bb79d2a40651bc9dbc5a1fa28642d22
Matt <97047080+mattxmade@users.noreply.github.com>
2024-06-19 23:44:56 +0100

    Implement model search [disable]; update module import

--- src/utils/faultSearch.ts
+++ src/utils/faultSearch.ts
@@ -1,12 +1,13 @@
 import { FaultItem } from "../types";
 import { BrandItem } from "../types";
 
-import brands from "../../data/brands";
+import brands from "../data/brands";
 
 const faultSearch = (string: string) => {
   const matches: FaultItem[] = [];
   const exactMatch: FaultItem[] = [];
   const brandMatches: FaultItem[] = [];
+  const modelMatches: FaultItem[] = [];
 
   const searchQuery = string.toLowerCase();
   const queryList = searchQuery.trim().split(/\s+/);
@@ -22,9 +23,17 @@ const faultSearch = (string: string) => {
         : queryList.some((query) => query === item.faultCode.toLowerCase()) &&
           matches.push(item);
     });
+
+    if (exactMatch.length || brandMatches.length) return;
+
+    // brand.faultCodes.forEach((item) => {
+    //   const model = item.model.toLowerCase();
+    //   model.includes(searchQuery) ? modelMatches.push(item) : null;
+    // });
   });
 
   if (exactMatch.length) return exactMatch;
+  if (modelMatches.length) return modelMatches;
   if (brandMatches.length) return brandMatches;
 
   return matches.length ? matches : null;
